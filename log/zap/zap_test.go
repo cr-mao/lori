42c26fc4d02a2662cedc4de3600a9d9e8c01fc5b
maozhongyu <crmao1991@gmail.com>
2023-05-11 16:24:42 +0800

    log 开发 from kratos

---
+++ log/zap/zap_test.go
@@ -0,0 +1,129 @@
+package zap
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
+	"github.com/cr-mao/lori/log"
+)
+
+type testWriteSyncer struct {
+	output []string
+}
+
+func (x *testWriteSyncer) Write(p []byte) (n int, err error) {
+	x.output = append(x.output, string(p))
+	return len(p), nil
+}
+
+func (x *testWriteSyncer) Sync() error {
+	return nil
+}
+
+// getEncoder 设置日志存储格式
+func getEncoder() zapcore.Encoder {
+
+	// 日志格式规则
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller", // 代码调用，
+		FunctionKey:    zapcore.OmitKey,
+		MessageKey:     "message",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,      // 每行日志的结尾添加 "\n"
+		EncodeLevel:    zapcore.CapitalLevelEncoder,    // 日志级别名称大写，如 ERROR、INFO
+		EncodeTime:     customTimeEncoder,              // 时间格式，我们自定义为 2006-01-02 15:04:05
+		EncodeDuration: zapcore.SecondsDurationEncoder, // 执行时间，以秒为单位
+		EncodeCaller:   zapcore.ShortCallerEncoder,     // Caller 短格式，如：types/converter.go:17，长格式为绝对路径
+	}
+
+	//// 本地环境配置
+	//if app.IsLocal() {
+	//	// 终端输出的关键词高亮
+	//	encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	//	// 本地设置内置的 Console 解码器（支持 stacktrace 换行）
+	//	return zapcore.NewConsoleEncoder(encoderConfig)
+	//}
+
+	// 线上环境使用 JSON 编码器
+	return zapcore.NewJSONEncoder(encoderConfig)
+}
+
+// customTimeEncoder 自定义友好的时间格式
+func customTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006-01-02 15:04:05"))
+}
+
+func TestDetailLogger(t *testing.T) {
+	writeSyncer := zapcore.AddSync(os.Stdout)
+	// 设置日志等级，具体请见 config/log.go 文件
+	logLevel := new(zapcore.Level)
+	if err := logLevel.UnmarshalText([]byte("info")); err != nil {
+		fmt.Println("日志初始化错误，日志级别设置有误。请修改 config/log.go 文件中的 log.level 配置项")
+	}
+	// 初始化 core
+	core := zapcore.NewCore(getEncoder(), writeSyncer, logLevel)
+	// 初始化 Logger
+	zlogger := zap.New(core,
+		zap.AddCaller(),                   // 调用文件和行号，内部使用 runtime.Caller
+		zap.AddCallerSkip(1),              // 封装了一层，调用文件去除一层(runtime.Caller(1))
+		zap.AddStacktrace(zap.ErrorLevel), // Error 时才会显示 stacktrace
+	)
+	// 将自定义的 logger 替换为全局的 logger
+	// zap.L().Fatal() 调用时，就会使用我们自定的 Logger
+	zap.ReplaceGlobals(zlogger)
+	logger := NewLogger(zlogger)
+	defer func() { _ = logger.Close() }()
+	zlog := log.NewHelper(logger)
+	zlog.Debugw("log", "debug")
+	zlog.Infow("log", "info")
+	zlog.Warnw("log", "warn")
+	zlog.Errorw("log", "error")
+	zlog.Errorw("log", "error", "except warn", "11")
+}
+
+func TestLogger(t *testing.T) {
+	syncer := &testWriteSyncer{}
+	encoderCfg := zapcore.EncoderConfig{
+		MessageKey:     "msg",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+	}
+	core := zapcore.NewCore(zapcore.NewJSONEncoder(encoderCfg), syncer, zap.DebugLevel)
+	zlogger := zap.New(core).WithOptions()
+	logger := NewLogger(zlogger)
+
+	defer func() { _ = logger.Close() }()
+
+	zlog := log.NewHelper(logger)
+
+	zlog.Debugw("log", "debug")
+	zlog.Infow("log", "info")
+	zlog.Warnw("log", "warn")
+	zlog.Errorw("log", "error")
+	zlog.Errorw("log", "error", "except warn")
+
+	except := []string{
+		"{\"level\":\"debug\",\"msg\":\"\",\"log\":\"debug\"}\n",
+		"{\"level\":\"info\",\"msg\":\"\",\"log\":\"info\"}\n",
+		"{\"level\":\"warn\",\"msg\":\"\",\"log\":\"warn\"}\n",
+		"{\"level\":\"error\",\"msg\":\"\",\"log\":\"error\"}\n",
+		"{\"level\":\"warn\",\"msg\":\"Keyvalues must appear in pairs: [log error except warn]\"}\n",
+	}
+	for i, s := range except {
+		if s != syncer.output[i] {
+			t.Logf("except=%s, got=%s", s, syncer.output[i])
+			t.Fail()
+		}
+	}
+}

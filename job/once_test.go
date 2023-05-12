efebe85e432361e05cc50a65fd3d52e54ddb480d
maozhongyu <crmao1991@gmail.com>
2023-05-13 00:01:52 +0800

    完善job单元测试

--- job/once_test.go
+++ job/once_test.go
@@ -2,7 +2,10 @@ package job
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestOnceJob(t *testing.T) {
@@ -19,4 +22,41 @@ func TestOnceJob(t *testing.T) {
 	} else {
 		t.Log("ok")
 	}
+}
+
+/*
+once job
+ERROR recover_err=Unknown Error, type: string, value: throw  exception
+    once_test.go:38: end
+*/
+func TestInterceptorsUnkownError(t *testing.T) {
+	onceJob := Once{
+		Work: func(ctx context.Context) error {
+			fmt.Println("once job")
+			panic("throw  exception")
+			return nil
+		},
+	}
+	onceJob.Run(context.Background())
+
+	time.Sleep(1 * time.Second)
+	t.Log("end")
+}
+
+/*
+once job
+ERROR recover_err=test error
+    once_test.go:51: end
+*/
+func TestInterceptorsWithError(t *testing.T) {
+	onceJob := Once{
+		Work: func(ctx context.Context) error {
+			fmt.Println("once job")
+			panic(errors.New("test error"))
+			return nil
+		},
+	}
+	onceJob.Run(context.Background())
+	time.Sleep(1 * time.Second)
+	t.Log("end")
 }
